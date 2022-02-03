ac97e8e3efda34a22bc4b519419e1f50f361b2c5
Jessica Organ <83175748+Jorgan612@users.noreply.github.com>
2022-02-03 16:26:23 -0700

    Add showDetails method to App.js

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,6 +3,7 @@ import '../CSS_styling/App.css';
 import movieData from '../movie-data';
 import Movies from './Movies';
 import NavBar from './NavBar';
+import Details from './Details'
 
 class App extends Component {
   constructor() {
@@ -13,12 +14,23 @@ class App extends Component {
    console.log(this.state.movieInfo)
   }
 
+  showDetails = (id) => {
+    if (event.target.id === id) {
+      return (
+        <Details 
+          movieInfo={this.state.movieInfo}
+          id={id}
+          />
+      )
+    }
+  }
+
   render() {
     return (
       <>
         <NavBar />
         <main>
-          <Movies movieInfos={this.state.movieInfo}/>
+          <Movies movieInfos={this.state.movieInfo} showDetails={this.showDetails}/>
         </main>
       </>
     )
