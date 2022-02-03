3c28e709a407ccbe102b824420c5cdd4eb39cee3
Stevie Ambroise <s.ambroise@protonmail.com>
2022-02-02 20:43:40 -0700

    Create file to display movie poster data

--- src/Components/Poster.js
+++ src/Components/Poster.js
@@ -3,11 +3,13 @@ import '../CSS_styling/Poster.css'
 
 const Poster = ({image, title, rating, releaseDate}) => {
   return (
-    <div>
-      <h1>{title}</h1>
+    <div className="poster">
       <img src={image} />
-      <h6>{rating}</h6>
-      <h6>{releaseDate}</h6>
+      <div className="poster-info">
+        <h4>Release Date:</h4>
+         <p>{releaseDate}</p>
+        <h5>Rating: {rating.toFixed(1)}</h5>
+      </div>
     </div>
   )
 }
