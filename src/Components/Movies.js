9d30f2a362bb56c84e0f1eaac36f0fca6c1c82a5
Jessica Organ <83175748+Jorgan612@users.noreply.github.com>
2022-02-03 16:26:51 -0700

    Add props to Movies Component

--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -3,16 +3,18 @@ import Poster from "./Poster";
 import '../CSS_styling/Movies.css';
 
 
-const Movies = ({movieInfos}) => {
+const Movies = ({movieInfos, showDetails}) => {
 console.log(movieInfos)
 
 let allPosters = movieInfos.map(movie => {
   return (
     <Poster 
+      id={movie["id"]}
       image={movie["poster_path"]}
       title={movie["title"]}
       rating={movie["average_rating"]}
       releaseDate={movie["release_date"]}
+      showDetails={showDetails(movie["id"])}
       />
   )
 })
