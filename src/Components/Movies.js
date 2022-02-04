c3d0a9eaebeab59d7b29d6c34a00eccce3d60210
Jessica Organ <83175748+Jorgan612@users.noreply.github.com>
2022-02-03 18:37:13 -0700

    Update props.

--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -3,18 +3,19 @@ import Poster from "./Poster";
 import '../CSS_styling/Movies.css';
 
 
-const Movies = ({movieInfos, showDetails}) => {
+const Movies = ({movieInfos, findDetails}) => {
 console.log(movieInfos)
 
 let allPosters = movieInfos.map(movie => {
   return (
     <Poster 
+      key={movie["id"]}
       id={movie["id"]}
       image={movie["poster_path"]}
       title={movie["title"]}
       rating={movie["average_rating"]}
       releaseDate={movie["release_date"]}
-      showDetails={showDetails(movie["id"])}
+      findDetails={findDetails(movie["id"])}
       />
   )
 })
