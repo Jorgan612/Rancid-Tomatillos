0476b1d02858510c339ac3ac6e82f700e6a9759a
Jessica Organ <83175748+Jorgan612@users.noreply.github.com>
2022-02-04 19:53:15 -0700

    Add classNames for css styling.

--- src/Components/Poster.js
+++ src/Components/Poster.js
@@ -4,11 +4,11 @@ import '../CSS_styling/Poster.css'
 const Poster = ({image, title, rating, releaseDate, findDetails, id}) => {
   return (
     <div className="poster">
-      <img src={image} alt={title} />
+      <img className='poster-img' src={image} alt={title} />
       <div className="poster-info">
         <h4>Release Date:</h4>
          <p>{releaseDate}</p>
-        <h5>Rating: {rating.toFixed(1)}</h5>
+        <h5 className='poster-rating'>Rating: {rating.toFixed(1)}</h5>
       </div>
       <div className="poster-button-div">
         <button className='poster-button' id={id} onClick={event => findDetails(event)}>i</button>
