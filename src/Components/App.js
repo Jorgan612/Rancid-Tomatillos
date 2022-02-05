f8fbe49055bb8bf327ea455f8fd7523c8e3c52a2
Stevie Ambroise <s.ambroise@protonmail.com>
2022-02-05 15:44:55 -0700

    Finagle with state and then fixed it

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,48 +1,28 @@
 import React, { Component } from 'react'
 import '../CSS_styling/App.css';
+import movieData from '../movie-data';
 import Movies from './Movies';
 import NavBar from './NavBar';
 import Details from './Details'
-import { fetchMovies } from '../apiCalls';
 
 class App extends Component {
   constructor() {
    super()
    this.state = {
-     movieInfo: []
+     movieInfo: movieData.movies
    }
    console.log("this.state.movieInfo here---", this.state.movieInfo)
   }
 
-  componentDidMount = () => {
-    fetchMovies()
-    .then(data => (this.setState({movieInfo: data.movies})))
-  }
-
   displayMainPage = () => {
-    this.componentDidMount()
+    this.setState({movieInfo: movieData.movies})
   }
-
-  //we need to utilize the id (event.target.id)
-  //and then pass it through a fetch call to make it dynamic
-  //and then do 2 .thens
-  //and then reset the state with that individual movi
-  //and then re-do our details poster thing with more info
 
   findDetails = (event) => {
     let id = Number(event.target.id)
-    console.log("number", id)
     fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
     .then(response => response.json())
     .then(data => this.setState({movieInfo: [data.movie]}))
-    .catch(err => console.log(err))
-    // this.state.movieInfo.find(movie => {
-    //   let id = Number(event.target.id)
-    //   if (movie.id === id) {
-    //   this.setState({movieInfo: [movie]})
-    //   return movie;
-    //   }
-    // })
   }
 
   render() {
