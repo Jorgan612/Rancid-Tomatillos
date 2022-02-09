da7987cbeb4231052f2d8641e438eeae44990112
Stevie Ambroise <s.ambroise@protonmail.com>
2022-02-08 19:28:17 -0700

    Change component from functional to class

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -1,29 +1,40 @@
-import React from 'react';
+import { React, Component } from 'react';
 import '../CSS_styling/Details.css';
+import {getSingleMovie} from '../apiCalls';
 
-const Details = ({movieInfo}) => {
-  let detail = movieInfo.map((movie) => {
+class Details extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movie: {}
+    }
+  }
+
+  componentDidMount() {
+    let id = parseInt(this.props.movieToRender.id)
+    getSingleMovie(id)
+    .then(data => {console.log("here we goooo!", data)
+      this.setState({movie: data.movie})})
+    .catch(error => console.log(error))
+  }
+
+  render() {
     return (
-        <div className='details-div'>
-          {movie['backdrop_path'] ? <img className='details-img' src={movie['backdrop_path']} alt={movie['title']}/> :
-          <img className='details-img' src={movie['poster_path']} alt={movie['title']} />}
-          <h1 className="movie-title">{movie['title']}</h1>
-          {movie.tagline && <h3 className='tagline'>"{movie['tagline']}"</h3>}
-          <h3 className='summary'>Summary:</h3> 
-          {movie.overview ? <p className='over-view'>{movie['overview']}</p> :
-          <p>No information available at this time. Come back soon.</p>}
-          {/* <h3 className='genre'>{movie['genre']}</h3>  */}
-          <h3 className='run-time-header'>Runtime:</h3>
-          {movie.runtime ? <p className='run-time'>{movie['runtime']} minutes</p> :
-          <p>No information available at this time. Come back soon.</p>}
-        </div>
-      )
-  })
-  return (
-    <div>
-      {detail}
-    </div>
-  )
+          <div className='details-div'>
+            {this.state.movie['backdrop_path'] ? <img className='details-img' src={this.state.movie['backdrop_path']} alt={this.state.movie['title']}/> :
+            <img className='details-img' src={this.state.movie['poster_path']} alt={this.state.movie['title']} />}
+            <h1 className="movie-title">{this.state.movie['title']}</h1>
+            {this.state.movie.tagline && <h3 className='tagline'>"{this.state.movie['tagline']}"</h3>}
+            <h3 className='summary'>Summary:</h3> 
+            {this.state.movie.overview ? <p className='over-view'>{this.state.movie['overview']}</p> :
+            <p>No information available at this time. Come back soon.</p>}
+            <h3 className='genre'>{this.state.movie['genre']}</h3>
+            <h3 className='run-time-header'>Runtime:</h3>
+            {this.state.movie.runtime ? <p className='run-time'>{this.state.movie['runtime']} minutes</p> :
+            <p>No information available at this time. Come back soon.</p>}
+          </div>
+     )
+  }
 }
 
 //img, tagline, title, overview, genre, runtime
