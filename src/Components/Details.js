a7d37c241059e45da6bf069a78b00e5b158fa1f1
Stevie Ambroise <s.ambroise@protonmail.com>
2022-02-08 19:37:32 -0700

    Clean up details file

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -37,37 +37,5 @@ class Details extends Component {
   }
 }
 
-//img, tagline, title, overview, genre, runtime
-//Tagline needs conditional
-
-// "release_date": "2020-07-02",
-// "overview": "A black ops assassin is forced to fight for her own survival after a job goes dangerously wrong.",
-// "genres": [
-// "Action",
-// "Crime",
-// "Drama",
-// "Thriller"
-// ],
-// "budget": 0,
-// "revenue": 152812,
-// "runtime": 96,
-// "tagline": "Kill. Or be killed.",
-// "average_rating": 5.142857142857143
 
 export default Details;
-
-//The button updates the state, so the method is going to have to live on the App,
-//the poster has a button that is going to take in a function, 
-//and so we are going to have to add the function to a prop key attribute when we invoke movies
-//Ex <Movie attribute1=whatever showDetails={this.showDetails}
-//In our movies component, also call showDetails in the dconstructed form 
-//to be able to pass it into the poster for a method on click
-
-//METHOD
-//iunno????
-//Lives in the app
-//needs to reset the state based on a find
-//event.target.id 
-//method needs to take in id as a parameter
-//if event.target id matches the id being put in the parameter
-//find that movie and then render those details BASED on the data found
