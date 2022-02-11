1c7690fa9987363d2bca2a330c1bed7c12ef120a
Jessica Organ <83175748+Jorgan612@users.noreply.github.com>
2022-02-11 13:56:07 -0700

    Add tests for Details card.

--- cypress/integration/detailsCard_specs.js
+++ cypress/integration/detailsCard_specs.js
@@ -10,14 +10,22 @@ describe('Details Page', () => {
     cy.get('img')
     .should('exist')
   })
+
+  it('User should be able to see a movie title', () => {
+    cy.get('.movie-title').contains('Money Plane')
+  })
+
+  it('User should be able to see a tagline', () => {
+    // cy.get('.tagline').contains('')
+  })
+
+  it('User should be able to see an overview', () => {
+    cy.get('.over-view').contains(`A professional thief with $40 million in debt and his family's life on the line must commit one final heist - rob a futuristic airborne casino filled with the world's most dangerous criminals.`)
+  })
+
+  it('User should be able to see a runtime', () => {
+    cy.get('.run-time').contains(82)
+  })
+  // check that main page button re-routes? Maybe just checking that it clicks?
 })
-
-//test for image
-//title
-//tagline
-//overview
-//runtime
-
-
-//check that main page button re-routes? Maybe just checking that it clicks?
 
