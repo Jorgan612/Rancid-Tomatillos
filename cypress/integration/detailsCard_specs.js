49cabb7a5d66ec3d78d35b7f1917c367293a6c0c
Stevie Ambroise <s.ambroise@protonmail.com>
2022-02-11 13:23:47 -0700

    Commence testing for details card

---
+++ cypress/integration/detailsCard_specs.js
@@ -0,0 +1,23 @@
+describe('Details Page', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/*', {fixture: 'details.json'}).as('getMovies')
+
+    cy.visit("http://localhost:3000/")
+    .get('poster-button').click()
+  })
+
+  it('User should be able to see movie image', () => {
+    cy.get('img')
+    .should('exist')
+  })
+})
+
+//test for image
+//title
+//tagline
+//overview
+//runtime
+
+
+//check that main page button re-routes? Maybe just checking that it clicks?
+
