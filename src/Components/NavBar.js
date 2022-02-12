8bb95acecc8686fb7708abe18514efd984b47ba9
Jessica Organ <83175748+Jorgan612@users.noreply.github.com>
2022-02-11 21:10:53 -0700

    Update NavBar with search functionality.

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,5 +1,6 @@
 import { React,  Component } from "react";
 import '../CSS_styling/NavBar.css';
+import { NavLink } from 'react-router-dom'
 
 class NavBar extends Component {
   constructor() {
@@ -8,32 +9,38 @@ class NavBar extends Component {
       searchedWord: ''
     }
   }
+
+  submitWord = (event) =>  {
+    const searchPhrase = {...this.state}
+    this.props.searchMovie(searchPhrase)
+  }
   
   handleChange = (event) => {
     this.setState({searchedWord: event.target.value})
-    console.log('this.state.searchedWord---',this.state.searchedWord)
   }
 
 
 render() {
-return (
-    <nav className="nav-bar">
-      <h1>Rancid Tomatillos</h1>
-      <div>
-      <label>Find A Movie</label>
-      <input 
-      id="inputBox"
-      type="text" 
-      name='search-box' 
-      value={this.state.searchedWord}
-      placeholder="Search for a movie by title..." 
-      onChange={event => this.handleChange(event)}
-      />
-      <button>Find A Movie</button>
-      </div>
-    </nav>
-  )
-}
+  return (
+      <nav className="nav-bar">
+        <h1>Rancid Tomatillos</h1>
+        <div>
+        <label>Find A Movie</label>
+        <input 
+        id="inputBox"
+        type="text" 
+        name='search-box' 
+        value={this.state.searchedWord}
+        placeholder="Search for a movie by title..." 
+        onChange={event => this.handleChange(event)}
+        />
+        <NavLink to="/filtered">
+          <button onClick={event => this.submitWord(event)}>Find A Movie</button>
+        </NavLink>
+        </div>
+      </nav>
+    )
+  }
 }
 
 export default NavBar;
