9167c3df2fd43f3ca31dddedce18b6dc85954d14
Jessica Organ <83175748+Jorgan612@users.noreply.github.com>
2022-02-11 19:36:22 -0700

    Update NavBar to class component, add state and input field and button for search functionality.

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,12 +1,39 @@
-import React from "react";
+import { React,  Component } from "react";
 import '../CSS_styling/NavBar.css';
 
-const NavBar = () => {
-  return (
-      <nav className="nav-bar">
-        <h1>Rancid Tomatillos</h1>
-      </nav>
+class NavBar extends Component {
+  constructor() {
+    super();
+    this.state = {
+      searchedWord: ''
+    }
+  }
+  
+  handleChange = (event) => {
+    this.setState({searchedWord: event.target.value})
+    console.log('this.state.searchedWord---',this.state.searchedWord)
+  }
+
+
+render() {
+return (
+    <nav className="nav-bar">
+      <h1>Rancid Tomatillos</h1>
+      <div>
+      <label>Find A Movie</label>
+      <input 
+      id="inputBox"
+      type="text" 
+      name='search-box' 
+      value={this.state.searchedWord}
+      placeholder="Search for a movie by title..." 
+      onChange={event => this.handleChange(event)}
+      />
+      <button>Find A Movie</button>
+      </div>
+    </nav>
   )
+}
 }
 
 export default NavBar;
