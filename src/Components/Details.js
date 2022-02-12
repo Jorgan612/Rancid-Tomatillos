b255b9d95128ee99a72d94b85a289513e96b45a0
Jessica Organ <83175748+Jorgan612@users.noreply.github.com>
2022-02-11 17:48:53 -0700

    Add button to poster details card.

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -1,6 +1,7 @@
 import { React, Component } from 'react';
 import '../CSS_styling/Details.css';
 import {getSingleMovie} from '../apiCalls';
+import {NavLink} from 'react-router-dom'
 
 class Details extends Component {
   constructor() {
@@ -24,14 +25,21 @@ class Details extends Component {
             {this.state.movie['backdrop_path'] ? <img className='details-img' src={this.state.movie['backdrop_path']} alt={this.state.movie['title']}/> :
             <img className='details-img' src={this.state.movie['poster_path']} alt={this.state.movie['title']} />}
             <h1 className="movie-title">{this.state.movie['title']}</h1>
-            {this.state.movie.tagline && <h3 className='tagline'>"{this.state.movie['tagline']}"</h3>}
+            {this.state.movie.tagline ? <h3 className='tagline'>Tagline: "{this.state.movie['tagline']}"</h3> : <h3 className='tagline'>Tagline: none.</h3>}
             <h3 className='summary'>Summary:</h3> 
             {this.state.movie.overview ? <p className='over-view'>{this.state.movie['overview']}</p> :
             <p>No information available at this time. Come back soon.</p>}
             <h3 className='genre'>{this.state.movie['genre']}</h3>
-            <h3 className='run-time-header'>Runtime:</h3>
-            {this.state.movie.runtime ? <p className='run-time'>{this.state.movie['runtime']} minutes</p> :
-            <p>No information available at this time. Come back soon.</p>}
+            <div className='runtime-mainpage-button-div'>
+              <h3 className='run-time-header'>Runtime:</h3>
+              {this.state.movie.runtime ? <p className='run-time'>{this.state.movie['runtime']} minutes</p> :
+              <p>No information available at this time. Come back soon.</p>}
+              <div className="nav-button-div">
+                <NavLink to="/">
+                  <button>Main Page</button>
+                </NavLink>
+              </div>
+            </div>
           </div>
      )
   }
