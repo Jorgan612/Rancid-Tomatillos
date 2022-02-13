6781642a00df852f77823a3ff5043d0c05050223
Jessica Organ <83175748+Jorgan612@users.noreply.github.com>
2022-02-12 19:43:20 -0700

    Update error handling in apicalls.

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,6 +1,19 @@
 export const fetchMovies = () => {
   return fetch("https://rancid-tomatillos.herokuapp.com/api/v2/movies/")
-  .then(response => response.json())
+  .then(response => {
+    if(!response.ok) {
+      if(response.status >= 500) {
+        throw new Error(`${response.status} ${response.messageText}. I am so sorry for the inconvenience! 
+        The server is down. Please try again, later.`)
+      } else if (response.status === 404) {
+        throw new Error(`${response.status} ${response.messageText}. We apologize for the inconvenience.
+        Please try again, or go back to the Main Page.`)
+      } else {
+        throw new Error(`${response.status} ${response.messageText}. We apologize for the inconvenience.
+        Please try again later.`)
+      }
+    }
+  })
 }
 
 export const getSingleMovie = (id) => {
